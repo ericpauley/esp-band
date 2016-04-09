13d68dbb7827f3b893fcfcce153abe164f66c787
Eric Pauley <eric@pauley.me>
2016-04-09 16:46:45 -0400

    Good times

--- src_node/index.js
+++ src_node/index.js
@@ -4,6 +4,21 @@ var app = express();
 var http = require('http').Server(app);
 app.use(express.static('html'));
 
+var redis = require("redis"),
+sender = redis.createClient();
+receiver = redis.createClient();
+
+receiver.on("ready", function(){
+  receiver.subscribe("waveform.*")
+  receiver.subscribe("pulse.*")
+})
+
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
