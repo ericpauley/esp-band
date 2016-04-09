fddcea4f37d71f3fc5dbf9168cef9827cc305bc2
Eric Pauley <eric@pauley.me>
2016-04-09 17:20:31 -0400

    Waveform listening with publish to clients

--- src_node/index.js
+++ src_node/index.js
@@ -9,11 +9,16 @@ sender = redis.createClient();
 receiver = redis.createClient();
 
 receiver.on("ready", function(){
-  receiver.subscribe("waveform.*")
-  receiver.subscribe("pulse.*")
+  receiver.psubscribe("waveform.*")
+  receiver.psubscribe("pulse.*")
 })
 
 var io = require('socket.io')(http);
+
+receiver.on('pmessage', function (pattern, channel, message) {
+  console.log(channel, message)
+  io.emit({channel:channel,message:message})
+});
 
 io.on('connection', function(socket){
   console.log('a user connected');
