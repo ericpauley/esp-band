bcd47a57d63fc1601d76a60abb69f501e3739947
Eric Pauley <eric@pauley.me>
2016-04-10 10:54:16 -0400

    Waves

--- src_node/python_proxy.py
+++ src_node/python_proxy.py
@@ -11,15 +11,10 @@ last = 0
 s.bind(("0.0.0.0", 1337))
 while True:
     data, addr = s.recvfrom(1024)
-    t, datagroup, value = [int(i) for i in data.split()]
-    if offset == 0 or time.time() > last+10:
-        offset = int(time.time()*1000-t)
-        last = time.time()
-    message = {"time":t+offset,"val":value}
-    print "ayy"
+    datagroup, value = [int(i) for i in data.split()]
+    message = {"time":int(time.time()*1000),"val":value}
     if datagroup == 0:
         r.publish("waveform.bill", json.dumps(message))
-        print("WAVEFORM")
     if datagroup == 1:
         r.publish("bpm.bill", json.dumps(message))
     if datagroup == 2:
